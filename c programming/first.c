c1b47ed01f6af537a4ec061923e137020938d6cd
varun <varun.oxford5@gmail.com>
2026-01-23 12:14:27 +0530

    add code

---
+++ c programming/first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("hey this is my first class of c programming");
+    return 0;
+}
+
---
+++ c programming/variables.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    // Variable Declaration and Initialization
+    // Input and Output Functions
+    // Format Specifiers
+    // Data Types in C
+
+    // int age;
+    // printf("pls enter your age : ");
+    // scanf("%d", &age);
+    // printf("your age is : %d", age);
+
+
+    // variables assignment and re-assignment
+    // int age = 23;
+    // age = 33;
+    // printf("%d",age);
+
+    // constant
+    const int age = 23;
+    // age = 33; // cannot re-assign value to const variable 
+    printf("%d",age);
+
+    return 0;
+}
+
